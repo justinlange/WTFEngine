ad29d3b4572ac1c017b6c329418978e67d0b179f
Justin Lange <julange@gmail.com>
2014-08-08 21:02:34 -0400

    takinhg out init

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AlvVpJApPj-LdDM3aUNJZmxlcVRZZFUtRVZfWkxaMnc' );
+    //WTF.init( '0AlvVpJApPj-LdDM3aUNJZmxlcVRZZFUtRVZfWkxaMnc' );
 
 });
